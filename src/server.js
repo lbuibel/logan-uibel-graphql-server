904f59c6f1144377ac66bb9b9119a68f0646d094
Logan Uibel <lbuibel@gmail.com>
2020-08-13 22:57:29 -0600

    added api key

--- src/server.js
+++ src/server.js
@@ -5,7 +5,13 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-new ApolloServer({ schema, context: createContext }).listen( { port: process.env.PORT || 4000}, () =>
+new ApolloServer({ 
+  schema, context: createContext,
+  engine: {
+    reportSchema: true
+  }
+})
+.listen( { port: process.env.PORT || 4000}, () =>
   console.log(
     `🚀 Server ready at: http://localhost:${process.env.PORT}\n⭐️ See sample queries: http://pris.ly/e/js/graphql-sdl-first#3-using-the-graphql-api`,
   ),
